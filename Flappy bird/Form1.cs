995c8fc52c54198bd620ed12dbbecf17a4dce01d
JovanS1212 <2021.09.17@online.devetagimnazija.edu.rs>
2023-11-10 14:04:35 +0100

    Dodat kod za ponovno pokretanje igre, napravljene promenljive brzine cevi i pticice, dodat score i highscore

--- Flappy bird/Form1.cs
+++ Flappy bird/Form1.cs
@@ -12,36 +12,83 @@ namespace Flappy_bird
 		{
 			InitializeComponent();
 		}
-		public static int pticaX;
-		public static int pticaY;
 		public static int razmak;
 		public static List<Cev> Cevi = new List<Cev>();
+		Label brojacPoena,maxPoena,krajIgre;
+
+		int brzinaPtice = 6;
+		int jacinaSkoka = 1;
+
+		int brzinaCevi = 3;
 
 		int indexPrveCevi;
 		int indexCeviIspredPtice;
 		int ideGore;
 		int ubrzavanje;
-		int Highscore;
+		int highScore;
+		int score;
 		bool gameOver;
 		private void Form1_Load(object sender, EventArgs e)
 		{
+
+			this.Width = 900;
+			this.Height = 500;
+
+			brojacPoena = new Label();
+			brojacPoena.BackColor = Color.White;
+			brojacPoena.ForeColor = Color.Black;
+			brojacPoena.BorderStyle = BorderStyle.Fixed3D;
+			brojacPoena.Text = "0";
+			brojacPoena.Font = new Font("Arial",30,FontStyle.Bold);
+			brojacPoena.SetBounds(Width/2-20,Height/10,40,45);
+			Controls.Add(brojacPoena);
+
+			krajIgre = new Label();
+			krajIgre.BackColor = Color.White;
+			krajIgre.ForeColor = Color.Black;
+			krajIgre.BorderStyle = BorderStyle.FixedSingle;
+			krajIgre.Text = "KRAJ IGRE";
+			krajIgre.Font = new Font("Gill Sans Ultra Bold", 50, FontStyle.Bold);
+			krajIgre.SetBounds(Width / 2-250, Height / 3, 500, 90);
+			Controls.Add(krajIgre);
+
+			maxPoena = new Label();
+			maxPoena.BackColor = Color.White;
+			maxPoena.ForeColor = Color.Black;
+			maxPoena.BorderStyle = BorderStyle.Fixed3D;
+			maxPoena.Font = new Font("Arial", 30, FontStyle.Bold);
+			maxPoena.SetBounds(Width / 2 - 100, Height / 3+100, 200, 45);
+			Controls.Add(maxPoena);
+
+			PocetnaPozicija();
+		}
+		private void PocetnaPozicija()
+		{
+			maxPoena.Visible = false;
+			krajIgre.Visible = false;
 			ideGore = 0;
 			ubrzavanje = 0;
 			gameOver = false;
+			score = 0;
+
+			brojacPoena.Text = "0";
+			brojacPoena.SetBounds(Width / 2 - 20, Height / 10, 40, 45);
 
 			var x = Width;
-			var sirinaCevi = Width/8;
-			razmak = (Width - 3*sirinaCevi)/3;
-			pticaX = Width / 8;
-			pticaY = Height / 2;
+			var sirinaCevi = Width / 8;
+			razmak = (Width - 3 * sirinaCevi) / 3;
 			PostaviPticu();
-			for ( int i = 0; i < 4; i++ )
+            if (Cevi.Count == 0)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+					Cevi.Add(new Cev(this));
+                }
+            }
+			for (int i = 0; i < 4; i++)
 			{
-				var cev = new Cev(this);
-				cev.PostaviRupu(x, sirinaCevi);
-				Cevi.Add(cev);
-
-				x += razmak+sirinaCevi;
+                Cevi[i].PostaviRupu(x, sirinaCevi);
+				x += razmak + sirinaCevi;
 			}
 			indexPrveCevi = 0;
 			indexCeviIspredPtice = 0;
@@ -50,44 +97,64 @@ namespace Flappy_bird
 		}
 		private void timer1_Tick(object sender, EventArgs e)
 		{
-            //Score i provera udarca
-            if (pbxPtica.Right >= Cevi[indexCeviIspredPtice].GornjaCev.Left && pbxPtica.Left <= Cevi[indexCeviIspredPtice].GornjaCev.Right)
+            if (!gameOver)
             {
-				if (pbxPtica.Top+5 <= Cevi[indexCeviIspredPtice].GornjaCev.Bottom ||
-					pbxPtica.Bottom-5 >= Cevi[indexCeviIspredPtice].DonjaCev.Top)
+				//Score i provera udarca
+				if (pbxPtica.Right >= Cevi[indexCeviIspredPtice].GornjaCev.Left && pbxPtica.Left <= Cevi[indexCeviIspredPtice].GornjaCev.Right)
 				{
-					gameOver = true;
+					if (pbxPtica.Top + 5 <= Cevi[indexCeviIspredPtice].GornjaCev.Bottom ||
+						pbxPtica.Bottom - 5 >= Cevi[indexCeviIspredPtice].DonjaCev.Top)
+					{
+						gameOver = true;
+					}
+				}
+				else if (pbxPtica.Left > Cevi[indexCeviIspredPtice].GornjaCev.Right)
+				{
+					indexCeviIspredPtice++;
+					score++;
+					if (score > highScore) highScore = score;
+					brojacPoena.Text = score.ToString();
+					int sirinaBrojaca = 40 + (30 * (brojacPoena.Text.Length-1));
+					brojacPoena.SetBounds(Width / 2 - sirinaBrojaca/2, Height / 10, sirinaBrojaca, 45);
+					if (indexCeviIspredPtice == Cevi.Count) indexCeviIspredPtice = 0;
+				}
+				PomeranjeCevi();
+				PomeranjePtice();
+			}
+            else
+            {
+				krajIgre.Visible = true;
+				maxPoena.Text = $"Rekord: {highScore}";
+				maxPoena.Width = 200 + brojacPoena.Width;
+				maxPoena.Visible = true;
+                if (pbxPtica.Top <= ClientSize.Height)
+                {
+					pbxPtica.Top += ubrzavanje;
+					if (ubrzavanje != brzinaPtice) ubrzavanje++;
 				}
             }
-			else if (pbxPtica.Left > Cevi[indexCeviIspredPtice].GornjaCev.Right)
-            {
-				indexCeviIspredPtice++;
-
-				if (indexCeviIspredPtice == Cevi.Count) indexCeviIspredPtice = 0;
-            }
-			//pomeranje cevi
-			if(!gameOver)PomeranjeCevi();
-			//kretanje ptice
-			if (ideGore > 0 && !gameOver)
+		}
+		private void PomeranjePtice()
+        {
+			if (ideGore > 0)
 			{
 				ubrzavanje = 0;
 				pbxPtica.Top -= ideGore;//koristim trajanje koliko dugo treba da ide gore da bi postigao blago usporavanje
 				ideGore--;
 			}
-			else if(pbxPtica.Top <= ClientSize.Height)
-            {
-				pbxPtica.Top+=ubrzavanje;
-				if(ubrzavanje !=5)ubrzavanje++;//blago ubrzavanje
-            }
+			else if (pbxPtica.Top <= ClientSize.Height)
+			{
+				pbxPtica.Top += ubrzavanje;
+				if (ubrzavanje != brzinaPtice) ubrzavanje++;//blago ubrzavanje
+			}
 			else gameOver = true;
-
 		}
 
 		private void PomeranjeCevi()
         {
 			foreach (var cev in Cevi)
 			{
-				cev.PomeriCevLevo(5);
+				cev.PomeriCevLevo(brzinaCevi);
 			}
 			if (Cevi[indexPrveCevi].DaLiJeIzaslo(razmak))
 			{
@@ -102,22 +169,21 @@ namespace Flappy_bird
 		}
 		private void PostaviPticu()
 		{
+			int pticaX = Width / 8;
+			int pticaY = Height / 2;
 			//pocetna pozicija i velicina ptice
-			int sirinaPtice = Width /12;
+			int sirinaPtice = Width /14;
 			int duzinaPtice = sirinaPtice - sirinaPtice/10;
 			pbxPtica.SetBounds(pticaX, pticaY-duzinaPtice, sirinaPtice, duzinaPtice);
 		}
-
-		private void pictureBox1_Click(object sender, EventArgs e)
-		{
-
-		}
-
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-			if (e.KeyCode == Keys.Space)
+			if (e.KeyCode == Keys.Space && !gameOver)
             {
-				ideGore = 10;//koliko dugo ce ici ka gore nakon pritiska spejsa
+				ideGore = brzinaPtice*jacinaSkoka;//koliko dugo ce ici ka gore nakon pritiska spejsa
+            }else if (e.KeyCode == Keys.Space)
+            {
+				PocetnaPozicija();
             }
         }
     }
