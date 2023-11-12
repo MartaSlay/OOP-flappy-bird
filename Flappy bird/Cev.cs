965c2a31ce8797663981952d181af0a6e3047f65
Dejan Grujić <dejan@cogin.com>
2023-11-12 22:19:49 +0100

    Popravljene labele i pod tako da odgovaraju visini i duzini prozora, napravljen Paint za pbxCevi(gornje i donje)

--- Flappy bird/Cev.cs
+++ Flappy bird/Cev.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Flappy_bird
 {
@@ -21,17 +23,68 @@ namespace Flappy_bird
 			this.form = form;
 			GornjaCev.TabStop = false;
 			DonjaCev.TabStop = false;
-			GornjaCev.Image = Image.FromFile("gornjaCev.png");
-			DonjaCev.Image = Image.FromFile("donjaCev.png");
+			DonjaCev.Paint += new System.Windows.Forms.PaintEventHandler(DonjaCev1_Paint);
+			GornjaCev.Paint += new System.Windows.Forms.PaintEventHandler(GornjaCev1_Paint);
+
 			GornjaCev.BackColor = Color.Transparent;
 			DonjaCev.BackColor = Color.Transparent;
-			DonjaCev.SizeMode = PictureBoxSizeMode.StretchImage;
-			GornjaCev.SizeMode = PictureBoxSizeMode.StretchImage;
 			
 			form.Controls.Add( GornjaCev );
 			form.Controls.Add( DonjaCev );
 		}
+		private void DonjaCev1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+		{
+			Graphics g = e.Graphics;
+			var ukupnaVisinaCevi = form.ClientSize.Height; 
+			var visinaGornjegDela = ukupnaVisinaCevi/12;
+			var visinaDonjegDela = ukupnaVisinaCevi - visinaGornjegDela;
+			var Xdonje = sirinaCevi/14;
+			var sirinaDonje = sirinaCevi-Xdonje*2;
+			var sirinaSenke = sirinaCevi/5;
 
+			Color zelena = ColorTranslator.FromHtml("#7FCD4A");
+			Color senka = ColorTranslator.FromHtml("#32531C");
+			Color senka2 = ColorTranslator.FromHtml("#4C7E29");
+
+			//donji deo cevi
+			g.FillRectangle(new SolidBrush(zelena), new Rectangle(Xdonje, visinaGornjegDela, sirinaDonje, visinaDonjegDela));
+			g.FillRectangle(new SolidBrush(senka), new Rectangle(Xdonje, visinaGornjegDela, sirinaSenke, visinaDonjegDela));
+			g.FillRectangle(new SolidBrush(senka2), new Rectangle(Xdonje+sirinaSenke, visinaGornjegDela, sirinaSenke, visinaDonjegDela));
+			g.FillRectangle(new SolidBrush(senka), new Rectangle(Xdonje, visinaGornjegDela, sirinaDonje, visinaGornjegDela/4));
+			g.DrawRectangle(new Pen(Color.Black), new Rectangle(Xdonje, visinaGornjegDela, sirinaDonje, visinaDonjegDela));
+			//gornji deo cevi
+			g.FillRectangle(new SolidBrush(zelena), new Rectangle(0, 0, sirinaCevi, visinaGornjegDela));
+			g.FillRectangle(new SolidBrush(senka), new Rectangle(0, 0, sirinaSenke, visinaGornjegDela));			
+			g.FillRectangle(new SolidBrush(senka2), new Rectangle(sirinaSenke, 0, sirinaSenke, visinaGornjegDela));			
+			g.DrawRectangle(new Pen(Color.Black), new Rectangle(0, 0, sirinaCevi, visinaGornjegDela));
+		}
+		private void GornjaCev1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+		{
+			Graphics g = e.Graphics;
+			var ukupnaVisinaCevi = form.ClientSize.Height;
+			var visinaDonjegDela = ukupnaVisinaCevi/12;
+			var visinaGornjegDela = ukupnaVisinaCevi - visinaDonjegDela;
+			var Xgornje = sirinaCevi/14;
+			var sirinaGornje = sirinaCevi-Xgornje*2;
+			var sirinaSenke = sirinaCevi/5;
+
+			Color zelena = ColorTranslator.FromHtml("#7FCD4A");
+			Color senka = ColorTranslator.FromHtml("#32531C");
+			Color senka2 = ColorTranslator.FromHtml("#4C7E29");
+
+			//gornji deo cevi
+			g.FillRectangle(new SolidBrush(zelena), new Rectangle(Xgornje, 0, sirinaGornje, visinaGornjegDela));
+			g.FillRectangle(new SolidBrush(senka), new Rectangle(Xgornje, 0, sirinaSenke, visinaGornjegDela));
+			g.FillRectangle(new SolidBrush(senka2), new Rectangle(Xgornje+sirinaSenke, 0, sirinaSenke, visinaGornjegDela));
+			g.FillRectangle(new SolidBrush(senka), new Rectangle(Xgornje, visinaGornjegDela-visinaDonjegDela/4, sirinaGornje, visinaDonjegDela/4));
+			g.DrawRectangle(new Pen(Color.Black), new Rectangle(Xgornje, 0, sirinaGornje, visinaGornjegDela));
+			//gornji deo cevi
+			g.FillRectangle(new SolidBrush(zelena), new Rectangle(0, visinaGornjegDela, sirinaCevi, visinaDonjegDela));
+			g.FillRectangle(new SolidBrush(senka), new Rectangle(0, visinaGornjegDela, sirinaSenke, visinaDonjegDela));
+			g.FillRectangle(new SolidBrush(senka2), new Rectangle(sirinaSenke, visinaGornjegDela, sirinaSenke,	visinaDonjegDela));
+			g.DrawRectangle(new Pen(Color.Black), new Rectangle(0, visinaGornjegDela, sirinaCevi, visinaDonjegDela));
+		}
+		public PaintEventHandler PaintEvent { get; set; }
 		public void PostaviRupu( int cevX, int sirinaCevi )
 		{
 			X = cevX;
@@ -42,8 +95,7 @@ namespace Flappy_bird
 			int razmakIzmedjuGornjeIDonje = visinaForm/3;
 			var random = new Random();
 
-			var cevYdole = random.Next(visinaForm/10+razmakIzmedjuGornjeIDonje, 
-					visinaForm - visinaForm/10);
+			var cevYdole = random.Next(visinaForm/10+razmakIzmedjuGornjeIDonje, visinaForm - visinaForm/6);
 			var cevYgore = cevYdole - razmakIzmedjuGornjeIDonje - duzinaCevi;
 
 
